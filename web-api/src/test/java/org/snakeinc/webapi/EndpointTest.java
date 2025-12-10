94bf40a58159eec9758f3e061d5209adde831a51
AGachelin <159674898+AGachelin@users.noreply.github.com>
2025-12-10 15:03:18 +0100

    Add tests

---
+++ web-api/src/test/java/org/snakeinc/webapi/EndpointTest.java
@@ -0,0 +1,35 @@
+package org.snakeinc.webapi;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.snakeinc.webapi.entity.Player;
+import org.snakeinc.webapi.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(classes = ApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EndpointTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Test
+    void testPostRequest() {
+        Player player =  new Player("test", 60);
+        ResponseEntity<Player> response = restTemplate.postForEntity("/api/v1/players/", player, Player.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assertions.assertEquals(response.getBody().getId(),player.getId());
+    }
+
+    @Test
+    void testGetRequest() {
+        restTemplate.postForEntity("/api/v1/players/", new Player("test", 60), Player.class);
+        ResponseEntity<Player> response = restTemplate.getForEntity("/api/v1/players/0", Player.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assertions.assertEquals(response.getBody().getId(),0);
+    }
+}
--- web-api/src/test/java/org/snakeinc/webapi/WebApiApplicationTests.java
+++ web-api/src/test/java/org/snakeinc/webapi/WebApiApplicationTests.java
@@ -1,13 +1,36 @@
 package org.snakeinc.webapi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.snakeinc.webapi.entity.Player;
+import org.snakeinc.webapi.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest @ExtendWith(MockitoExtension.class)
 class WebApiApplicationTests {
+    @InjectMocks
+    private PlayerService playerService;
+    @Test
+    void testCreatePlayer() {
+        Player player = new Player("test", 14);
+        Assertions.assertEquals("test", player.getName());
+        Assertions.assertEquals(14, player.getAge());
+    }
 
     @Test
-    void contextLoads() {
+    void testPlayerService() {
+        Player player = new Player("test", 14);
+        playerService.createPlayer(player);
+        Player player2 = playerService.getPlayer(player.getId());
+        Assertions.assertNotNull(player2);
+        Assertions.assertEquals("test", player2.getName());
+        Assertions.assertEquals(14, player2.getAge());
     }
 
 }
+
